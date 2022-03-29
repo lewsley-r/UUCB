c1678ddfa3c398822a59ba3aeed6ba0c8a04f1ef
Ronan Lewsley <61333546+lewsley-r@users.noreply.github.com>
2022-03-29 19:54:40 +0100

    Delete backend/infrastructure directory

--- backend/infrastructure/python/bookcloth/utils.py
+++
@@ -1,6 +0,0 @@
-def input_default(inp, default, use_defaults=False):
-    if use_defaults:
-        print('%s [%s]: %s' % (inp, default, default))
-        return default
-    else:
-        return input('%s [%s]: ' % (inp, default)) or default
--- backend/infrastructure/templates/redis.env.tpl
+++
@@ -1,1 +0,0 @@
-REDIS_PASS={{ redis['pass'] }}
